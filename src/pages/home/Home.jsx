f3d811f1b954fb55b0481dbe6d57f30546dfa5e2
jaehanbyun <awbrg789@naver.com>
2023-08-16 20:40:27 +0900

    Create Project and First Setting

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+import Widget from '../../components/widget/Widget'
+import './home.scss'
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,12 @@
+import './list.scss'
+
+const List = () => {
+    return (
+        <div>
+            List
+        </div>
+    )
+}
+
+export default List
+
