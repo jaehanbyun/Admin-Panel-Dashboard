6d50f52863396125e9d24586688e9a7302d1dc47
jaehanbyun <awbrg789@naver.com>
2023-08-16 20:57:56 +0900

    Add Sidebar Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,6 +7,9 @@ const Home = () => {
     return (
         <div className='home'>
             <Sidebar />
+            <div className="homeContainer">
+                <Navbar />
+            </div>
         </div>
     )
 }
